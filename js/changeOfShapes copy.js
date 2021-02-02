c93d3ab96a83326736c92441af2c79740fdfcfaa
Temyper <pall47g1@gmail.com>
2021-02-02 22:58:50 +0900

    [Complete]

--- js/changeOfShapes copy.js
+++ js/changeOfShapes copy.js
@@ -11,14 +11,10 @@ class Shape {
     this.#shape = shape;
   }
 
-  get Item() {
-    return this.#shape;
-  }
-
   set Left(value) {
     if (!this.#TestForPX(value.toString())) {
       console.log(
-        `${value} isn't appropriate for ${this.X.name} that this isn't "integer'px'" `
+        `${value} isn't appropriate for ${this.Left.name} that this isn't "integer'px'" `
       );
       return;
     }
@@ -30,7 +26,7 @@ class Shape {
   set Top(value) {
     if (!this.#TestForPX(value.toString())) {
       console.log(
-        `${value} isn't appropriate for ${this.Y.name} that this isn't "integer'px'" `
+        `${value} isn't appropriate for ${this.Top.name} that this isn't "integer'px'" `
       );
       return;
     }
@@ -49,25 +45,28 @@ class Shape {
     return this.#shape.css("top");
   }
 
-  // 弧度
   Rotate(angle) {
-    angle %= 360;
+    // 20210201負の値でも正しく余りが出る（ex:(-361) % 360 = -1）
     this.#shape.css({ transform: `rotate(${angle}deg)` });
   }
 
-  // 20210201コールバック関数がprivateなメンバーを参照できる非同期メソッド。setIntervalはwindowオブジェクトに属するので、外部のオブジェクトのprivateなメンバーを参照できない。
+  // 20210201コールバック関数がprivateなメンバーを参照できる非同期メソッド。setIntervalはwindowオブジェクトに属するので、windowオブジェクトの外部のオブジェクトのprivateなメンバーを参照できない。
   async RotateInfinitely(angle) {
+    if (angle == 0) return;
+
+    let totalAngle = angle;
     while (true) {
-      this.Rotate(angle);
-      angle++;
+      totalAngle %= 360;
+      this.Rotate(totalAngle);
+      totalAngle += angle;
       await this.#Sleep(50);
     }
   }
 
   Move(plusX, plusY) {
     // parseIntだけでpxが外れ整数値を求められる
-    this.Left = `${parseInt(this.Left) - plusX}px`;
-    this.Top = `${parseInt(this.Top) - plusY}px`;
+    this.Left = `${parseInt(this.Left) + plusX}px`;
+    this.Top = `${parseInt(this.Top) + plusY}px`;
   }
 
   async MoveInfinitely(plusX, plusY) {
@@ -89,7 +88,12 @@ class Shape {
   }
 }
 
-let shape = new Shape($("#shape1"));
+let shape1 = new Shape($("#shape1"));
 
-shape.RotateInfinitely(1);
-shape.MoveInfinitely(2, 1);
+shape1.RotateInfinitely(1);
+shape1.MoveInfinitely(2, 1);
+
+let shape2 = new Shape($("#shape2"));
+
+shape2.RotateInfinitely(-1);
+shape2.MoveInfinitely(-1, -2);
--- js/changeOfShapes.js
+++ js/changeOfShapes.js
@@ -5,16 +5,33 @@
 class Shape {
   #shape = null;
 
-  #patternOfPX = /^-?\d{1,}px/;
+  #patternOfPX = /^-?\d{1,}\.?\d{0,}px$/;
+
+  // #statusOperatedBymouse = {
+  //   isBeingDragged: false,
+
+  //   shapePositionBeformouseDown: null,
+
+  //   cursorPositonBeformouseDown: { left: 0, top: 0 }
+  // };
 
   constructor(shape) {
     this.#shape = shape;
+    // this.#shape.mousedown(e => {
+    //   this.#mouseDown(e);
+    // });
+    // this.#shape.mouseup(e => {
+    //   this.#mouseUp(e);
+    // });
+    // this.#shape.mousemove(e => {
+    //   this.#mouseMove(e);
+    // });
   }
 
-  set Left(value) {
-    if (!this.#TestForPX(value.toString())) {
+  set left(value) {
+    if (!this.#testForPX(value.toString())) {
       console.log(
-        `${value} isn't appropriate for ${this.Left.name} that this isn't "integer'px'" `
+        `${value} isn't appropriate for ${this.left.name} that this isn't "integer'px'" `
       );
       return;
     }
@@ -23,10 +40,10 @@ class Shape {
     });
   }
 
-  set Top(value) {
-    if (!this.#TestForPX(value.toString())) {
+  set top(value) {
+    if (!this.#testForPX(value.toString())) {
       console.log(
-        `${value} isn't appropriate for ${this.Top.name} that this isn't "integer'px'" `
+        `${value} isn't appropriate for ${this.top.name} that this isn't "integer'px'" `
       );
       return;
     }
@@ -36,64 +53,89 @@ class Shape {
   }
 
   // 戻り値の末尾はpxがある
-  get Left() {
+  get left() {
     return this.#shape.css("left");
   }
 
   // 戻り値の末尾はpxがある
-  get Top() {
+  get top() {
     return this.#shape.css("top");
   }
 
-  Rotate(angle) {
+  rotate(angle) {
     // 20210201負の値でも正しく余りが出る（ex:(-361) % 360 = -1）
     this.#shape.css({ transform: `rotate(${angle}deg)` });
   }
 
   // 20210201コールバック関数がprivateなメンバーを参照できる非同期メソッド。setIntervalはwindowオブジェクトに属するので、windowオブジェクトの外部のオブジェクトのprivateなメンバーを参照できない。
-  async RotateInfinitely(angle) {
+  async rotateInfinitely(angle) {
     if (angle == 0) return;
 
     let totalAngle = angle;
     while (true) {
       totalAngle %= 360;
-      this.Rotate(totalAngle);
+      this.rotate(totalAngle);
       totalAngle += angle;
-      await this.#Sleep(50);
+      await this.#sleep(50);
     }
   }
 
-  Move(plusX, plusY) {
+  move(plusX, plusY) {
     // parseIntだけでpxが外れ整数値を求められる
-    this.Left = `${parseInt(this.Left) + plusX}px`;
-    this.Top = `${parseInt(this.Top) + plusY}px`;
+    this.left = `${parseInt(this.left) + plusX}px`;
+    this.top = `${parseInt(this.top) + plusY}px`;
   }
 
-  async MoveInfinitely(plusX, plusY) {
+  async moveInfinitely(plusX, plusY) {
     while (true) {
-      this.Move(plusX, plusY);
-      await this.#Sleep(50);
+      this.move(plusX, plusY);
+      await this.#sleep(50);
     }
   }
 
   // 20210201privateなメンバーを参照できる、時間指定用非同期実行メソッド。setIntervalではコールバック関数がprivateなメンバーを参照できない。
-  #Sleep(seconds) {
+  #sleep(seconds) {
     return new Promise(resolve => {
       setTimeout(resolve, seconds);
     });
   }
 
-  #TestForPX(string) {
+  #testForPX(string) {
     return this.#patternOfPX.test(string);
   }
+
+  // #mouseDown(e) {
+  //   this.#statusOperatedBymouse.isBeingDragged = true;
+  //   this.#shape.css({ backgroundColor: "#ff0000" });
+  //   this.#statusOperatedBymouse.shapePositionBeformouseDown = this.#shape.position();
+  //   this.#statusOperatedBymouse.cursorPositionBeformouseDown = {
+  //     left: e.clientX,
+  //     top: e.clientY
+  //   };
+  // }
+
+  // #mouseUp(e) {
+  //   this.#statusOperatedBymouse.isBeingDragged == false;
+  //   this.#shape.css({ backgroundColor: "#0000ff" });
+  // }
+
+  // // 20210201幾つかのサイトでpositionをabsoluteにすることを推奨されるが、それをすると図形の回転時に他の要素の位置と鬩ぎ合う
+  // #mouseMove(e) {
+  //   if (this.#statusOperatedBymouse.isBeingDragged == false) return;
+
+  //   this.left =
+  //     this.#statusOperatedBymouse.shapePositionBeformouseDown.left +
+  //     (e.clientX -
+  //       this.#statusOperatedBymouse.cursorPositionBeformouseDown.left);
+  // }
 }
 
 let shape1 = new Shape($("#shape1"));
 
-shape1.RotateInfinitely(1);
-shape1.MoveInfinitely(2, 1);
+shape1.rotateInfinitely(1);
+shape1.moveInfinitely(2, 1);
 
 let shape2 = new Shape($("#shape2"));
 
-shape2.RotateInfinitely(-1);
-shape2.MoveInfinitely(-1, -2);
+shape2.rotateInfinitely(-1);
+shape2.moveInfinitely(-1, -2);
